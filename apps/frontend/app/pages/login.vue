9d2dfbd5ab26fdff4094c12342df90132fd69123
Neeil Gupta <neeilgupta12@gmail.com>
2026-03-05 11:13:17 -0500

    Fixed composable issue with login.vue

--- apps/frontend/app/pages/login.vue
+++ apps/frontend/app/pages/login.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { useRouter } from "vue-router";
-import { useAuth } from "../../../composables/useAuth";
+import { useAuth } from "../../composables/useAuth";
 
 const router = useRouter();
 const { requestCode, verifyCode, register, passwordLogin } = useAuth();
